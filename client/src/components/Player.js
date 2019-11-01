f6b8dd2412f02f5df8c1fcca948873de8b077264
Insurikai <40343703+Insurikai@users.noreply.github.com>
2019-11-01 11:15:56 -0700

    created player component

---
+++ client/src/components/Player.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import useSavePlayer from '../hooks/useSavePlayer';
+
+export default class Player extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            player: this.props.player
+        }
+    }
+    render(){
+        return(
+            <button className="player" onClick={()=>{this.props.saveToLocal(this.state.player)}}>
+                <h1 className="name">{this.state.player.name}</h1>
+                <p className="country">{this.state.player.country}</p>
+            </button>
+        );
+    }
+}
